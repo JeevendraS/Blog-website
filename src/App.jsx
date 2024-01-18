cffc56c14096794ba373b87e7db8ff103e4e6bda
Jeevendra <jeevendrasingh1999@gmail.com>
2024-01-18 17:59:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import configVariable from './configVariable/configVariable'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/Auth'
+import { Header,Footer } from './components'
+import { login, logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='text-2xl bg-red-500'>hello Jeevendra singh</h1>
-    </>
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block '>
+        <Header/>
+          <main>
+            TODO: <Outlet/>
+          </main>
+        <Footer/>
+      </div>
+    </div>
+  ):(
+    <div>loading....</div>
   )
 }
 
