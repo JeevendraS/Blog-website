62f92b352e0c67f91eea081f060775239fbbca12
Jeevendra <jeevendrasingh1999@gmail.com>
2024-02-03 17:33:34 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
         <nav className='flex'>
           <div className='mr-4'>
             <Link to='/'>
-              <Logo width='70px'/>
+              <Logo width='70px' />
             </Link>
           </div>
           <ul className=' flex ml-auto'>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+// import image from './src/assets/logo-color.png'
 
 function Logo({width='100px'}) {
+
   return (
-    <div>Logo</div>
+    <div >
+      <img src={image} alt="" />
+    </div>
   )
 }
 
